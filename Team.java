841cc8cf8a81dfe0a6cab6b613682e411b819a30
Ivan <ivanpiccioni.work@gmail.com>
2023-11-23 11:54:49 +0100

    branch ivan

--- Team.java
+++ Team.java
@@ -2,6 +2,9 @@ public class Team {
 
     public static void main(String[] args) {
 
+        System.out.println("Ivan, Piccioni, 28 , Roma" +
+                "videogiochi , Pasta , Dark Souls 3 , Lord of the rign"+
+                "lord of the ring, Athena");
 
 
     }
