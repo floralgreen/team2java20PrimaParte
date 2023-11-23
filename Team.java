3c89efdd97c96c343f701d8cd0f0040611d0268e
MariaNappi <nappimaria12@gmail.com>
2023-11-23 20:55:59 +0100

    Aggiorno le informazioni

--- Team.java
+++ Team.java
@@ -2,7 +2,23 @@ public class Team {
 
     public static void main(String[] args) {
     
-            System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
+            
+        System.out.println("Ivan, Piccioni, 28 , Roma" +
+                "videogiochi , Pasta , Dark Souls 3 , Lord of the rign"+
+                "lord of the ring, Athena");
+
+        System.out.println("Stefano,Forcinito,33,Fabrica di Roma,Calcetto,Pizza,Fifa,Fast and Furius,Piccoli Brividi," +
+               "Milo");
+
+        System.out.println("Federico, Carducci, 23, Frosinone," +
+                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
+                " Uno Nessuno Centomila, Mokaccina");
+
+        System.out.println("Luigi, Mottola, 25, Lusciano, " +
+                "Table Top Gaming, Pizza, Killzone 2, The Lord of the Rings (Original Trilogy), " +
+                "The Lord of the Rings, Macchia");
+            
+        System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
                 "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");
     }
 }
