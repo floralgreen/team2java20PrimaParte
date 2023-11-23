1a110d4c84d6bdbaff44b4220de2947a69d9285d
Stefano <stegallo1990@gmail.comgit config -lgit config --global user.email stegallo1990@gmail.com>
2023-11-23 11:45:57 +0100

    aggiunto nuovo branch

--- Team.java
+++ Team.java
@@ -1,8 +1,8 @@
 public class Team {
 
     public static void main(String[] args) {
-
-
+       System.out.println("Stefano,Forcinito,33,Fabrica di Roma,Calcetto,Pizza,Fifa,Fast and Furius,Piccoli Brividi," +
+               "Milo");
 
     }
 
