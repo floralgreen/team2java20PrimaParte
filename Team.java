6d5fefea2ec6839519583ad858c656801ea3f930
MariaNappi <nappimaria12@gmail.com>
2023-11-23 20:46:52 +0100

    sono qui

--- Team.java
+++ Team.java
@@ -1,8 +1,8 @@
 public class Team {
 
     public static void main(String[] args) {
-
-        System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims,
-          Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");
+    
+            System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
+                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");
     }
 }
