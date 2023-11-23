f77f913105d0fef57134ba8c46e80ca1341fc8c5
floralgreen <carducci.federico20@gmail.com>
2023-11-23 11:32:20 +0100

    Creato branch Federico e aggiunte info nel println

--- Team.java
+++ Team.java
@@ -2,7 +2,9 @@ public class Team {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Federico, Carducci, 23, Frosinone," +
+                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
+                " Uno Nessuno Centomila, Mokaccina");
 
     }
 
