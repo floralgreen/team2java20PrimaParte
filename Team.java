a7e88d4a3554463e3f359d409d844207c123934c
LuigiMottola <sputnick427@gmail.com>
2023-11-23 11:43:19 +0100

    added luigi branch

--- Team.java
+++ Team.java
@@ -1,9 +1,11 @@
 public class Team {
 
     public static void main(String[] args) {
-
-
-
+        System.out.println("Luigi, Mottola, 25, Lusciano, " +
+                "Table Top Gaming, Pizza, Killzone 2, The Lord of the Rings (Original Trilogy), " +
+                "The Lord of the Rings, Macchia");
     }
+
+
 
 }
