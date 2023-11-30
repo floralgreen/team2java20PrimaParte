0094265f1229042c3275edda95df74c7d1789cfe
floralgreen <carducci.federico20@gmail.com>
2023-11-30 11:07:11 +0100

    sistemato Main creato array Team che deve contenere gli oggetti studenti con le nostre info

--- src/Team.java
+++ src/Team.java
@@ -1,8 +1,10 @@
 public class Team {
 
     public static void main(String[] args) {
-    
-            
+
+        Studente[] team = new Studente[5];
+
+
         System.out.println("Ivan, Piccioni, 28 , Roma" +
                 "videogiochi , Pasta , Dark Souls 3 , Lord of the rign"+
                 "lord of the ring, Athena");
@@ -18,39 +20,8 @@ public class Team {
                 "Table Top Gaming, Pizza, Killzone 2, The Lord of the Rings (Original Trilogy), " +
                 "The Lord of the Rings, Macchia");
 
-
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-        System.out.println("Federico, Carducci, 23, Frosinone," +
-                " Music, Pasta, Bloodborne, Arancia Meccaninca," +
-                " Uno Nessuno Centomila, Mokaccina");
-
-
-        System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");       System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
-                "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");
-
         System.out.println("Maria, Nappi, 27, Molfetta, gym, gelato, The Sims" +
                 "Indiana Jones, Mio nonno era un ciliegio, Cuoricino di panna");
+
     }
 }
